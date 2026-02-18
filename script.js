685a4a603d35d625ac415123a6d7f0dcc2d04a5e
RyotaKise09 <122669853+RyotaKise09@users.noreply.github.com>
2026-02-17 21:41:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -447,7 +447,7 @@ if (continueBtn) {
       guests
     };
 
-   fetch("https://script.google.com/macros/s/AKfycbz-QyZA9X7ivn7LMvI00ziy2bplWcz8NTggaHd7TlihX2Zfwj8aZaRVgSdFpnoj9FeOFQ/exec", {
+   fetch("https://script.google.com/macros/s/AKfycbzmhUTGb_ewmsbqZbaLPxIo5w-yCHGBSjR4MNfm5sgorLspvdCm4FyQ7ecKqlxGCE2dpQ/exec", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
@@ -468,6 +468,7 @@ if (continueBtn) {
     window.location.href = `confirmation.html?guests=${guestsParam}`;
   });
 }
+
 
 
 
